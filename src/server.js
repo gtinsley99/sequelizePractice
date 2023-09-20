95c03aedb6c9f4b327ca3d5b70459bd1baf2c141
George Tinsley <george8s@live.co.uk>
2023-09-20 11:48:20 +0100

    add Book model server.js

--- src/server.js
+++ src/server.js
@@ -8,6 +8,23 @@ const app = express();
 
 app.use(express.json());
 
+const { DataTypes } = require("sequelize");
+const connection = require(".db/connection");
+
+const Book = connection.define("Book", {
+    title: {
+        type: DataTypes.STRING,
+        unique: true,
+        allowNull: false,
+    },
+    author: {
+        type: DataTypes.STRING
+    },
+    genre: {
+        type: DataTypes.STRING
+    }
+});
+
 // http://localhost/health
 app.get("/health", (req, res) => {
     res.status(200).json({message: "API is healthy."});
