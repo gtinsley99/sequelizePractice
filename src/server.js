719af07379da77e015335a871522c47efc3dfab2
George Tinsley <george8s@live.co.uk>
2023-09-20 10:51:00 +0100

    add basic server

--- src/server.js
+++ src/server.js
@@ -1,1 +1,11 @@
+const express = require("express");
 
+const port = 80;
+
+const app = express();
+
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log(`App is listening on port ${port}.`);
+});
