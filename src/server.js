6d4ff4f06f8cb39310edd254efba0328537d66a8
George Tinsley <george8s@live.co.uk>
2023-09-20 11:13:55 +0100

    add health route server.js

--- src/server.js
+++ src/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 
+// 80 is default so not needed in local host url
 const port = 80;
 
 const app = express();
 
 app.use(express.json());
+
+// http://localhost/health
+app.get("/health", (req, res) => {
+    res.status(200).json({message: "API is healthy."});
+});
 
 app.listen(port, () => {
     console.log(`App is listening on port ${port}.`);
