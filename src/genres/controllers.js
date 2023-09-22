e19e70e64e16291fba35edbe2c9dbdf82df783ff
George Tinsley <141220209+gtinsley99@users.noreply.github.com>
2023-09-22 10:40:28 +0100

    add genre not found to getgenre route genres/controller.js working

--- src/genres/controllers.js
+++ src/genres/controllers.js
@@ -2,87 +2,95 @@ const Genre = require("./model");
 const Book = require("../books/model");
 
 const addGenre = async (req, res) => {
-    try {
-        console.log(req.body);
-            const genre = await Genre.create({
-                genre: req.body.genre
-            });
-            res.status(201).json({
-                genre: genre,
-                message: "Genre added"
-            });
-    } catch (error) {
-        console.log(error);
-        res.status(501).json({
-            message: "Error occurred",
-            error: error,
-        });
-    }
+  try {
+    console.log(req.body);
+    const genre = await Genre.create({
+      genre: req.body.genre,
+    });
+    res.status(201).json({
+      genre: genre,
+      message: "Genre added",
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: "Error occurred",
+      error: error,
+    });
+  }
 };
 
 const deleteGenre = async (req, res) => {
-    try {
-        const genre = await Genre.findOne({where: {genre: req.body.genre}});
-        if (!genre){
-            res.status(404).json({
-                success: false,
-                message: "Genre not found",
-                genre: req.body.genre,
-            });
-        } else {
-            await genre.destroy();
-            res.status(200).json({
-                message: "Genre deleted",
-                genre: genre,
-            });
-        };
-    } catch (error) {
-        console.log(error);
-        res.status(501).json({
-            message: "Error occurred",
-            error: error,
-        });
-    };
+  try {
+    const genre = await Genre.findOne({ where: { genre: req.body.genre } });
+    if (!genre) {
+      res.status(404).json({
+        success: false,
+        message: "Genre not found",
+        genre: req.body.genre,
+      });
+    } else {
+      await genre.destroy();
+      res.status(200).json({
+        message: "Genre deleted",
+        genre: genre,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: "Error occurred",
+      error: error,
+    });
+  }
 };
 
 const getAllGenres = async (req, res) => {
-    try {
-        const getAllGenres = await Genre.findAll({});
-        res.status(200).json({
-            message: "Success",
-            genres: getAllGenres,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(501).json({
-            message: "Error occurred",
-            error: error,
-        });
-    };
+  try {
+    const getAllGenres = await Genre.findAll({});
+    res.status(200).json({
+      message: "Success",
+      genres: getAllGenres,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: "Error occurred",
+      error: error,
+    });
+  }
 };
 
-const getGenre =async (req, res) => {
-    try {
-        const getGenre = await Genre.findOne({where: {genre: req.body.genre}});
-        let id = getGenre.id;
-        const getBooks = await Book.findAll({where: {GenreId: id}});
-        res.status(200).json({
-            message: "Success",
-            genre: getGenre,
-            books: getBooks,
-        });
-    } catch (error) {
-        console.log(error);
-        res.status(501).json({
-            message: "Error occurred",
-            error: error,
-        });
-    };
+const getGenre = async (req, res) => {
+  try {
+    const getGenre = await Genre.findOne({ where: { genre: req.body.genre } });
+    if (!getGenre) {
+      res.status(404).json({
+        success: false,
+        message: "Genre not found",
+        genre: req.body.genre,
+      });
+    } else {
+      let id = getGenre.id;
+      const getBooks = await Book.findAll({ where: { GenreId: id } });
+      res.status(200).json({
+        message: "Success",
+        genre: getGenre,
+        books: getBooks,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(501).json({
+      message: "Error occurred",
+      error: error,
+    });
+  }
 };
 
 module.exports = {
-    addGenre,
-    deleteGenre,
-    getAllGenres,
-    getGenre,
-}
+  addGenre,
+  deleteGenre,
+  getAllGenres,
+  getGenre,
+};
